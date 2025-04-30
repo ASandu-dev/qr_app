2fc200ff67645a5d4cfc6a001a00e045eca2c450
@AS <andrei.sandu@computerminds.co.uk>
2025-04-30 18:00:03 +0100

    deployment changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/qr_app', // optional, if deploying to repo URL
 };
 
 export default nextConfig;
