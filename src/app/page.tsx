f404479e18270b59386e9d97541fc37f78c5e358
@AS <andrei.sandu@computerminds.co.uk>
2025-05-01 19:40:17 +0100

    improved design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
     <Container maxWidth="lg" 
       className="bg-gray-300 h-screen flex flex-col items-center justify-center"
     >
-      <Box className="bg-white w-[90%] md:w-[30%] rounded-3xl shadow-lg p-6 flex flex-col items-center">
+      <Box className="bg-white w-[85%] md:w-[30%] rounded-3xl shadow-lg p-6 flex flex-col items-center">
         <Image 
           src={`${prefix}/assets/images/image-qr-code.png`} 
           alt="QR Code" 
@@ -21,7 +21,7 @@ const Home = () => {
         <h1 className="text-2xl font-bold text-center text-gray-800 mt-4">
           Improve your front-end skills by building projects
         </h1>
-        <p className="text-gray-300 text-sm text-center mt-2">
+        <p className="text-gray-400 text-md text-center mt-2">
           Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
         </p>
       </Box>
